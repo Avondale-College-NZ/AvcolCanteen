cae08e21bdc699e6965613205385a9600fcbde76
Muhammad Amar <ac116681@avcol.school.nz>
2024-09-10 18:10:31 +1200

    admin links

--- Areas/Identity/Data/AvcolCanteenUser.cs
+++ Areas/Identity/Data/AvcolCanteenUser.cs
@@ -15,6 +15,8 @@ public class AvcolCanteenUser : IdentityUser
     [RegularExpression("^[A-Z][a-zA-Z]*$", ErrorMessage = "The first letter must be capitalised and only letters are allowed")] // defines a specific way of entering data
     [Required] // requires user to fill in 
     public string FirstName { get; set; }
+    [StringLength(30, ErrorMessage = "Field must be less than 30 characters")] // limits the amount of data that can be entered
+    [RegularExpression("^[A-Z][a-zA-Z]*$", ErrorMessage = "The first letter must be capitalised and only letters are allowed")] // defines a specific way of entering data
     [Required] // requires user to fill in
     public string LastName { get; set; }
     public bool ChangePassword { get; set; } = false;
